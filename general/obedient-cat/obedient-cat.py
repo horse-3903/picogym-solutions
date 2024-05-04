ab531973343ce2c12c23885f77c527830b221b78
horse-3903 <chongchoonhourafael@gmail.com>
2024-05-04 15:39:01 +0800

    update : obedient-cat

--- general/obedient-cat/obedient-cat.py
+++ general/obedient-cat/obedient-cat.py
@@ -1,6 +1,52 @@
+import os
+import subprocess
+
+def run_bash(command: list[str]) -> tuple[str, str]:
+    command = " ".join(command)
+
+    print(f"Running bash command : `{command}`", end="...")
+    
+    p = subprocess.Popen(["bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    out, err = p.communicate(command.encode())
+
+    print("Done\n")
+    
+    try:
+        out, err = out.decode(), err.decode()
+    except:
+        pass
+
+    if not err:
+        print("Output received : ")
+        print(out or None)
+    else:
+        print("Error caught : ")
+        print(err)
+
+    print()
+
+    return out, err
+
+def password_found(pw: str) -> str:
+    name = __file__.split(".")[0]
+    
+    dir = f"{name}-flag.txt"
+
+    print(f"Password Found : {pw}\n")
+
+    print(f"Saving password to {dir}", end="...")
+    
+    with open(dir, "w+") as f:
+        f.write(pw)
+
+    print("Done\n")
+
+    return dir
+
 result = ""
 
-with open("./general/obedient_cat/flag", "r") as f:
+with open("./general/obedient-cat/flag", "r") as f:
     result = f.read()
 
-print(result)
+dir = password_found(result)
