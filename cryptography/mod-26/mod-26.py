a522d9f3080c39739c3d7103c978c21aaec784bc
horse-3903 <chongchoonhourafael@gmail.com>
2024-05-04 15:38:35 +0800

    update : mod-26

--- cryptography/mod-26/mod-26.py
+++ cryptography/mod-26/mod-26.py
@@ -1,4 +1,48 @@
-s = "cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_GYpXOHqX}"
+import os
+import subprocess
+
+def run_bash(command: list[str]) -> tuple[str, str]:
+    command = " ".join(command)
+
+    print(f"Running bash command : `{command}`", end="...")
+    
+    p = subprocess.Popen(["bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    out, err = p.communicate(command.encode())
+
+    print("Done\n")
+    
+    try:
+        out, err = out.decode(), err.decode()
+    except:
+        pass
+
+    if not err:
+        print("Output received : ")
+        print(out or None)
+    else:
+        print("Error caught : ")
+        print(err)
+
+    print()
+
+    return out, err
+
+def password_found(pw: str) -> str:
+    name = __file__.split(".")[0]
+    
+    dir = f"{name}-flag.txt"
+
+    print(f"Password Found : {pw}\n")
+
+    print(f"Saving password to {dir}", end="...")
+    
+    with open(dir, "w+") as f:
+        f.write(pw)
+
+    print("Done\n")
+
+    return dir
 
 def rot13(s: str) -> str:
     res = ""
@@ -13,4 +57,8 @@ def rot13(s: str) -> str:
 
     return res
 
-print(rot13(s))
+s = "cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_GYpXOHqX}"
+
+result = rot13(s)
+
+dir = password_found(result)
