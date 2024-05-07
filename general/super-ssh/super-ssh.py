48700362f74229ef622fb45d6f551e3a719c4138
horse-3903 <chongchoonhourafael@gmail.com>
2024-05-07 14:45:27 +0800

    add : super-ssh

---
+++ general/super-ssh/super-ssh.py
@@ -0,0 +1,58 @@
+import os
+import subprocess
+
+import pwn
+
+def run_bash(command: list[str]) -> tuple[str, str] | tuple[bytes, bytes]:
+    command = " ".join(command)
+
+    print(f"Running bash command : `{command}`", end="...")
+    
+    p = subprocess.Popen(["bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    out, err = p.communicate(command.encode())
+
+    print("Done\n")
+    
+    try:
+        out, err = out.decode("unicode-escape"), err.decode("unicode-escape")
+    except:
+        pass
+
+    if not err:
+        print("Output received : ")
+        print(out or None)
+    else:
+        print("Error caught : ")
+        print(err)
+
+    print()
+
+    return out, err
+
+def password_found(pw: str) -> str:
+    name = __file__.split(".")[0]
+    
+    dir = f"{name}-flag.txt"
+
+    print(f"Password Found : {pw}\n")
+
+    print(f"Saving password to {dir}", end="...")
+    
+    with open(dir, "w+") as f:
+        f.write(pw)
+
+    print("Done\n")
+
+    return dir
+
+ssh = pwn.ssh(user="ctf-player", host="titan.picoctf.net", port=60555, password="1ad5be0d")
+
+s = ssh.system("")
+
+content = s.recv()
+content = content.decode()
+
+result = content[content.find("picoCTF{"): content.find("}")+1]
+
+dir = password_found(result)
