036320350fd05ece85c2a5c15796994329dd0ab9
horse-3903 <chongchoonhourafael@gmail.com>
2024-05-07 14:32:19 +0800

    add : static-aint-always-noise

---
+++ general/static-aint-always-noise/static-aint-always-noise.py
@@ -0,0 +1,51 @@
+import os
+import subprocess
+
+def run_bash(command: list[str]) -> tuple[str, str] | tuple[bytes, bytes]:
+    command = " ".join(command)
+
+    print(f"Running bash command : `{command}`", end="...")
+    
+    p = subprocess.Popen(["bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    out, err = p.communicate(command.encode())
+
+    print("Done\n")
+    
+    try:
+        out, err = out.decode("unicode-escape"), err.decode("unicode-escape")
+    except:
+        pass
+
+    if not err:
+        print("Output received : ")
+        print(out or None)
+    else:
+        print("Error caught : ")
+        print(err)
+
+    print()
+
+    return out, err
+
+def password_found(pw: str) -> str:
+    name = __file__.split(".")[0]
+    
+    dir = f"{name}-flag.txt"
+
+    print(f"Password Found : {pw}\n")
+
+    print(f"Saving password to {dir}", end="...")
+    
+    with open(dir, "w+") as f:
+        f.write(pw)
+
+    print("Done\n")
+
+    return dir
+
+out, err = run_bash(["cat", "./general/static-aint-always-noise/static", "|", "grep", "-a", "picoCTF"])
+
+result = out[out.find("picoCTF{"): out.find("}")+1]
+
+dir = password_found(result)
