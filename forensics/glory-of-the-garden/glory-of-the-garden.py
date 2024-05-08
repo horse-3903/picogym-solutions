89fc8ccaf8706d7bd9fd0ee113a5e51ea1f5e30d
horse-3903 <chongchoonhourafael@gmail.com>
2024-05-08 13:28:57 +0800

    add : glory-of-the-garden

---
+++ forensics/glory-of-the-garden/glory-of-the-garden.py
@@ -0,0 +1,55 @@
+import os
+import subprocess
+
+def run_bash(command: list[str]) -> tuple[str, str] | tuple[bytes, bytes]:
+    command = " ".join(command)
+
+    print(f"Running bash command : `{command}`", end="...")
+    
+    p = subprocess.Popen(["bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    out, err = p.communicate(command.encode())
+
+    print("Done\n")
+    
+    try:
+        out, err = out.decode(), err.decode()
+    except:
+        pass
+
+    if not err:
+        print("Output received : ")
+        print(out or None)
+    else:
+        print("Error caught : ")
+        print(err)
+
+    print()
+
+    return out, err
+
+def password_found(pw: str) -> str:
+    name = __file__.split(".")[0]
+    
+    dir = f"{name}-flag.txt"
+
+    print(f"Password Found : {pw}\n")
+
+    print(f"Saving password to {dir}", end="...")
+    
+    with open(dir, "w+") as f:
+        f.write(pw)
+
+    print("Done\n")
+
+    return dir
+
+dir = "./forensics/glory-of-the-garden"
+
+out, err = run_bash(["cat", dir + "/garden.jpg", "|", "grep", "--binary-files=text", '"picoCTF{"'])
+
+out = out[out.rfind(b"picoCTF{"): out.rfind(b"}")+1]
+
+result = out.decode()
+
+dir = password_found(result)
