d353e69e21b36121ca9ad2059b2aed552d14e93a
horse-3903 <chongchoonhourafael@gmail.com>
2024-05-09 10:42:07 +0800

    add : scan-surprise

---
+++ forensics/scan-surprise/scan-surprise.py
@@ -0,0 +1,55 @@
+import subprocess
+
+from qreader import QReader
+import cv2
+
+def run_bash(command: list[str]) -> tuple[str, str] | tuple[bytes, bytes]:
+    command = " ".join(command)
+
+    print(f"Running bash command : `{command}`", end="...")
+    
+    p = subprocess.Popen(["bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    out, err = p.communicate(command.encode())
+
+    print("Done\n")
+    
+    try:
+        out, err = out.decode(), err.decode()
+    except:
+        pass
+
+    if not err:
+        print("Output received : ")
+        print(out or None)
+    else:
+        print("Error caught : ")
+        print(err)
+
+    print()
+
+    return out, err
+
+def password_found(pw: str) -> str:
+    name = __file__.split(".")[0]
+    
+    dir = f"{name}-flag.txt"
+
+    print(f"Password Found : {pw}\n")
+
+    print(f"Saving password to {dir}", end="...")
+    
+    with open(dir, "w+") as f:
+        f.write(pw)
+
+    print("Done\n")
+
+    return dir
+
+qreader = QReader()
+
+image = cv2.cvtColor(cv2.imread("./forensics/scan-surprise/flag.png"), cv2.COLOR_BGR2RGB)
+
+result = qreader.detect_and_decode(image=image)[0]
+
+dir = password_found(result)
