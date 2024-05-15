dca3a227f50cdf4cb428a41e4bf77f291629bbfb
horse-3903 <chongchoonhourafael@gmail.com>
2024-05-15 12:49:05 +0800

    add : blame-game

---
+++ general/blame-game/blame-game.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+sys.path.append(".")
+
+from util import *
+
+cur_dir = os.path.dirname(__file__).replace("\\", "/")
+cur_dir = "." + cur_dir.split("picogym-solutions")[-1]
+
+out, err = run_bash(["cd", cur_dir + "/drop-in", "&&", "git", "log"])
+
+out, err = run_bash(["cd", cur_dir + "/drop-in", "&&", "git", "log", "|", "grep", "picoCTF{"])
+
+result = out[out.find("picoCTF{"):out.find("}")+1]
+
+dir = save_password(result, __file__)
