352843dafe5b93aaed1fcb105982bb6e036108f8
horse-3903 <chongchoonhourafael@gmail.com>
2024-05-15 12:17:00 +0800

    update : util

--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import subprocess
+
 def save_password(pw: str, file: str) -> str:
     name = file.split(".")[0]
     
@@ -15,3 +17,30 @@ def save_password(pw: str, file: str) -> str:
     print("Done\n")
 
     return dir
+
+def run_bash(command: list[str]) -> tuple[str, str] | tuple[bytes, bytes]:
+    command = " ".join(command)
+
+    print(f"Running bash command : `{command}`", end="...")
+    
+    p = subprocess.Popen(["bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    out, err = p.communicate(command.encode())
+
+    print("Done\n")
+    
+    try:
+        out, err = out.decode("unicode-escape"), err.decode("unicode-escape")
+    except:
+        pass
+
+    if not err:
+        print("Output received : ")
+        print(out or None)
+    else:
+        print("Error caught : ")
+        print(err)
+
+    print()
+
+    return out, err
