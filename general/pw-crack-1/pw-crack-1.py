c06eed9284a79e2c24ee232a335bb7e5281b93e6
horse-3903 <chongchoonhourafael@gmail.com>
2024-11-17 17:28:37 +0800

    add : pw-crack-1

---
+++ general/pw-crack-1/pw-crack-1.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("")
+from util import save_flag, run_bash
+
+result = None
+
+out, err = run_bash(["echo", "691d", "|", "python3", "general/pw-crack-1/level1.py"])
+
+result = out
+
+dir = save_flag(result, __file__)
