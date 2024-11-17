fb051316721c796422211d82f0e32cffc54d4714
horse-3903 <chongchoonhourafael@gmail.com>
2024-11-17 17:20:36 +0800

    add : first-grep

---
+++ general/first-grep/first-grep.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append("")
+from util import save_flag, run_bash
+
+out, err = run_bash(["cat", "general/first-grep/file", "|", "grep", "picoCTF{"])
+
+result = out
+
+dir = save_flag(result, __file__)
