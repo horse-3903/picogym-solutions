07ff8fce1bb30a50ad493a8820fa61d5a275d28c
horse-3903 <chongchoonhourafael@gmail.com>
2024-11-17 18:12:07 +0800

    add : pw-crack-2

---
+++ general/pw-crack-2/pw-crack-2.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("")
+from util import save_flag, run_bash
+
+result = None
+
+pw = chr(0x33) + chr(0x39) + chr(0x63) + chr(0x65)
+
+out, err = run_bash(["echo", pw, "|", "python3", "general/pw-crack-2/level2.py"])
+
+result = out
+
+dir = save_flag(result, __file__)
