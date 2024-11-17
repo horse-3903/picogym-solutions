e558b701e760587ae531d4e54b9c292c1fb3de21
horse-3903 <chongchoonhourafael@gmail.com>
2024-11-17 17:16:16 +0800

    add : strings-it

---
+++ general/strings-it/strings-it.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append("")
+from util import save_flag, run_bash
+
+out, err = run_bash(["strings", "general/strings-it/strings", "|", "grep", "picoCTF{"])
+
+result = out
+
+dir = save_flag(result, __file__)
